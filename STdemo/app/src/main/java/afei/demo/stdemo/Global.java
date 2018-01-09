a7d10c774b86cf349007b48538339e6f8fe407ae
chaofei.wu <chaofei.wu.hz@tcl.com>
2018-01-09 11:51:08 +0800

    STdemo

---
+++ STdemo/app/src/main/java/afei/demo/stdemo/Global.java
@@ -0,0 +1,10 @@
+package afei.demo.stdemo;
+
+/**
+ * Created by chaofei on 17-12-28.
+ */
+
+public class Global {
+    public static Down mDown=null;
+    public static Thread mThread=null;
+}
---
+++ STdemo/app/src/main/java/afei/demo/stdemo/InterfaceCallback.java
@@ -0,0 +1,11 @@
+package afei.demo.stdemo;
+
+/**
+ * Created by chaofei on 17-12-7.
+ */
+
+public interface InterfaceCallback {
+
+     void showCallback(DataCallback data);
+
+}
